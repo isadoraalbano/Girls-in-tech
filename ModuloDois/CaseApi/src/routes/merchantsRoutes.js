3f19a6a2a6e5aeaa146e86958eb993a4a0857943
Vincenzo G Neto <vincenzovgn@gmail.com>
2022-05-16 14:30:33 -0300

    sugestão melhoria case-2

--- ModuloDois/CaseApi/src/routes/merchantsRoutes.js
+++ ModuloDois/CaseApi/src/routes/merchantsRoutes.js
@@ -1,17 +1,22 @@
-import express from 'express'
+import express from "express";
 
-import MerchantsController from '../controllers/MerchantsController.js'
+import MerchantsController from "../controllers/MerchantsController.js";
 
 const router = express.Router();
-
+/**
+ * Aqui nós temos o uso do padrão camelCase, o que está bom.
+ * Mas sugiro o uso do quebec-case para as rotas, pois nós devemos sempre lembrar que uma página de um site, sempre ou na maioria das vezes rsrs são em letra minúscula.
+ * então vamos adotar algo como: .put('/merchant/status/:id)
+ *
+ * Além disso as rotas de merchants não devem ser alterada, ou seja, seu recurso primário é a loja, ou seja, merchants e depois você tem os recursos dessa rota como atualização de status, ou seja, merchants/status/:id
+ */
 router
-    .get('/merchants', MerchantsController.findAll)
-    .get('/merchants/:id', MerchantsController.findById)
-    .post('/merchants', MerchantsController.createMerchant)
-    .put('/merchants/:id', MerchantsController.updateMerchant)
-    .put('/merchantStatus/:id', MerchantsController.updateStatus)
-    .put('/merchantMessage/:id', MerchantsController.sendMessage)
-    .delete('/merchant/:id', MerchantsController.deleteMerchant)
-
+  .get("/merchants", MerchantsController.findAll)
+  .get("/merchants/:id", MerchantsController.findById)
+  .post("/merchants", MerchantsController.createMerchant)
+  .put("/merchants/:id", MerchantsController.updateMerchant)
+  .put("/merchants/status/:id", MerchantsController.updateStatus)
+  .put("/merchants/message/:id", MerchantsController.sendMessage)
+  .delete("/merchants/:id", MerchantsController.deleteMerchant);
 
 export default router;
